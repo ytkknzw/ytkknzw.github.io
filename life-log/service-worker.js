b5f8bc97ae28f3b3bdb6b4fc5af045f98628a04a
Yutaka Kanazawa <work.yutaka.kanazawa@gmail.com>
2017-08-07 11:45:41 +0900

    a

--- life-log/service-worker.js
+++ life-log/service-worker.js
@@ -15,7 +15,7 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWidth(
+    event.respondWith(
         caches.match(event.request)
             .then(function (response) {
                 if (response) {
