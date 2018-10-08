e8c27628b82067b308ce71ac583c4f831e792109
Yutaka Kanazawa <reg.yutaka.kanazawa@gmail.com>
2018-10-08 17:48:14 +0900

    add sake

---
+++ sake/precache-manifest.b6fdbef29d94690f68d4d99d0efb49ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sake/robots.txt"
+  },
+  {
+    "revision": "ff2636703a3ad540d79c",
+    "url": "/sake/js/index.js"
+  },
+  {
+    "revision": "c862f565a4e19b7aec38",
+    "url": "/sake/js/chunk-vendors.js"
+  },
+  {
+    "revision": "a3342e29db92004b0109",
+    "url": "/sake/js/about.js"
+  },
+  {
+    "revision": "841b3e4ebbfc96692d8ed761f8f7876d",
+    "url": "/sake/index.html"
+  },
+  {
+    "revision": "ff2636703a3ad540d79c",
+    "url": "/sake/css/index.css"
+  }
+];
