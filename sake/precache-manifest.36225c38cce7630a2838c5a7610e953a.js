8a8bae0bc88ca49ebc3e51e1dcde142ef3a2e000
Yutaka Kanazawa <reg.yutaka.kanazawa@gmail.com>
2018-10-11 18:42:06 +0900

    a

---
+++ sake/precache-manifest.36225c38cce7630a2838c5a7610e953a.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/sake/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/sake/robots.txt"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/sake/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "19940b4a526040e073e3",
+    "url": "/sake/js/index.148611d1.js"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/sake/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "80c429390286f33d1e69",
+    "url": "/sake/js/chunk-vendors.efb3eec4.js"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/sake/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/sake/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/sake/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/sake/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/sake/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/sake/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/sake/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "879cc35873807dae563eadcabed7e743",
+    "url": "/sake/img/sort.png"
+  },
+  {
+    "revision": "caced6df46e79c84218351bf0b641d51",
+    "url": "/sake/index.html"
+  },
+  {
+    "revision": "852132ccae5273097be9c2fbc8a2300c",
+    "url": "/sake/img/shopping-bag.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/sake/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "b727a25b7e6d063b583eb9551b5e7655",
+    "url": "/sake/img/edit.png"
+  },
+  {
+    "revision": "de231fef4cb1153eeb05f1383d865452",
+    "url": "/sake/img/back.png"
+  },
+  {
+    "revision": "35114708a1be096c7f3cd21f9c93c94c",
+    "url": "/sake/img/gear.png"
+  },
+  {
+    "revision": "0e629f98f6293fbe2c6660593cb5fb44",
+    "url": "/sake/img/noImage.png"
+  },
+  {
+    "revision": "f830d1abbc220c12f2003c3ef53f4bc1",
+    "url": "/sake/img/placeholder.png"
+  },
+  {
+    "revision": "25837c788726e22710e1e9bafc5798d9",
+    "url": "/sake/img/search.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/sake/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/sake/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "19940b4a526040e073e3",
+    "url": "/sake/css/index.c6264642.css"
+  }
+];
--- sake/service-worker.js
+++ sake/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/sake/precache-manifest.1796cdcc49189b8bf9ef60aaa8d75528.js"
+  "/sake/precache-manifest.36225c38cce7630a2838c5a7610e953a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "japanese_sake"});
