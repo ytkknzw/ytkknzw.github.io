1f734a1aca73d516f4dcf7b9bbd917c6ddc61b7b
Yutaka Kanazawa <reg.yutaka.kanazawa@gmail.com>
2018-10-24 17:43:14 +0900

    add kawaya

---
+++ kawaya/precache-manifest.9bca59e1cc5cda00a7bf682706e4103f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "35114708a1be096c7f3cd21f9c93c94c",
+    "url": "/kawaya/img/gear.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kawaya/robots.txt"
+  },
+  {
+    "revision": "0e629f98f6293fbe2c6660593cb5fb44",
+    "url": "/kawaya/img/noImage.png"
+  },
+  {
+    "revision": "4aaf4778567feb3ffd11",
+    "url": "/kawaya/js/chunk-vendors.105edd5d.js"
+  },
+  {
+    "revision": "34f80a202ece3ab79c89",
+    "url": "/kawaya/js/app.570b8808.js"
+  },
+  {
+    "revision": "8c538d8bad9d89023a8eb8670d5e67e4",
+    "url": "/kawaya/index.html"
+  },
+  {
+    "revision": "879cc35873807dae563eadcabed7e743",
+    "url": "/kawaya/img/sort.png"
+  },
+  {
+    "revision": "852132ccae5273097be9c2fbc8a2300c",
+    "url": "/kawaya/img/shopping-bag.png"
+  },
+  {
+    "revision": "25837c788726e22710e1e9bafc5798d9",
+    "url": "/kawaya/img/search.png"
+  },
+  {
+    "revision": "f830d1abbc220c12f2003c3ef53f4bc1",
+    "url": "/kawaya/img/placeholder.png"
+  },
+  {
+    "revision": "b727a25b7e6d063b583eb9551b5e7655",
+    "url": "/kawaya/img/edit.png"
+  },
+  {
+    "revision": "de231fef4cb1153eeb05f1383d865452",
+    "url": "/kawaya/img/back.png"
+  },
+  {
+    "revision": "34f80a202ece3ab79c89",
+    "url": "/kawaya/css/app.cc7ffbdf.css"
+  }
+];
